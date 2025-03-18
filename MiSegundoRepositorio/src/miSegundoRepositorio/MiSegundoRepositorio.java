cd51fcc60ae489a9c6fe2e7bf373738cbd94f993
Alberto Dominguez Domingo <add743@educa.madrid.org>
2025-03-18 20:49:48 +0100

    Update MiSegundoRepositorio.java

--- MiSegundoRepositorio/src/miSegundoRepositorio/MiSegundoRepositorio.java
+++ MiSegundoRepositorio/src/miSegundoRepositorio/MiSegundoRepositorio.java
@@ -3,5 +3,7 @@ package miSegundoRepositorio;
 public class MiSegundoRepositorio {
 	public static void main(String[] args) {
 		System.out.println("Segundo repositorio primer commit");
+                System.out.println("Segundo repositorio segundo commit");
+
 	}
 }
